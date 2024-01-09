1a978307c57fe6b79de13c725942357b4cc88733
kimcodec <kuban2545@gmail.com>
2024-01-09 05:02:32 +0300

    сделано модальное окно и подсчет цены

--- main.js
+++ main.js
@@ -9,6 +9,18 @@ const rowsPerPage = 5;
 let currentRouteId = -1;
 let currentGuideId = -1;
 
+let DayOff = [
+    "01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07", "01-08",
+    "02-23",
+    "03-08",
+    "04-29", "04-30",
+    "05-01", "05-09", "05-10",
+    "06-12",
+    "11-04",
+    "12-30", "12-31"
+]
+
+
 function applyGuideOptionFilters(){
     let selector = document.getElementById("guideLanguagePlace");
     let selectedOption = selector.options[selector.selectedIndex];
@@ -98,13 +110,100 @@ function fillGuidesTable(){
     }
 }
 
-function buttonChooseClick(){
-    currentGuideId = event.target.getAttribute("guide-id");
-    openPurchaseModal();
+function clearModal(){
+    document.getElementById("routeStartTime").value = "";
+    document.getElementById("routeDate").value = "";
+    document.getElementById("peopleNumber").value = "";
+    document.getElementById("routeDuration").value = "1";
+    document.getElementById("firstOption").checked = "";
+    document.getElementById("firstOption").disabled = false;
+    document.getElementById("secondOption").checked = "";
 }
 
-function openPurchaseModel(){
+function buttonChooseClick(){
+    clearModal();
+    currentGuideId = event.target.getAttribute("guide-id");
+    fillPurchaseModal();
+    calculateAmount();
+}
 
+function calculateAmount(){
+    let amount = 0;
+
+    let firstOptionCoef = 1.15;
+    let secondOptionCoef = 1;
+    if (document.getElementById("secondOption").checked){
+        secondOptionCoef = 1.3;
+    }
+
+    let routeTime = document.getElementById("routeStartTime").value.split(":");
+    let routeTimeHours = parseInt(routeTime[0]);
+    let routeTimeMinutes = parseInt(routeTime[1]);
+    let morningPlus = 0, eveningPlus = 0;
+    if (routeTimeHours >= 9 && routeTimeHours <= 12){
+        if (!(routeTimeHours === 12 && routeTimeMinutes > 0)){
+            morningPlus = 400;
+        }
+    }
+    if (routeTimeHours >= 20 && routeTimeHours <= 23){
+        if (!(routeTimeHours === 23 && routeTimeMinutes > 0)){
+            morningPlus = 1000;
+        }
+
+    }
+    let dayOffCoef = 1;
+
+    let date = document.getElementById("routeDate").value;
+    DayOff.forEach(date=>{
+        if (date.toString().includes(date)){
+            dayOffCoef = 1.5;
+        }
+
+    });
+    let peopleNumber = parseInt(document.getElementById("peopleNumber").value);
+    let peoplePlus = 0;
+    if (peopleNumber > 5 && peopleNumber <= 10){
+        peoplePlus = 1000;
+        firstOptionCoef = 1.25;
+    } else if (peopleNumber > 10 && peopleNumber <= 20){
+        peoplePlus = 1500;
+
+    }
+    let selector = document.getElementById("routeDuration");
+
+    let routeDuration = parseInt(selector.options[selector.selectedIndex].value);
+    let pricePerHour = 0;
+    AllGuidesArray.forEach(guide =>{
+        if (guide.id == currentGuideId){
+            pricePerHour = parseInt(guide.pricePerHour);
+        }
+
+    });
+
+    amount = pricePerHour * routeDuration * dayOffCoef + morningPlus + eveningPlus + peoplePlus;
+    amount *= secondOptionCoef;
+    if (document.getElementById("firstOption").checked){
+        amount *= firstOptionCoef;
+    }
+
+    document.getElementById("totalAmount").innerText = Math.ceil(amount).toString();
+}
+
+function fillPurchaseModal(){
+    let routeName, guideName;
+    AllRoutesResponseArray.forEach(route =>{
+        if (route.id == currentRouteId){
+            routeName = route.name;
+        }
+    });
+
+    AllGuidesArray.forEach(guide =>{
+        if (guide.id == currentGuideId){
+            guideName = guide.name;
+        }
+    });
+    document.getElementById("modalGuideName").innerText = guideName;
+    document.getElementById("modalRouteName").innerText = routeName;
 }
 
 function fillOptionSelector(){
@@ -276,6 +375,15 @@ function getRoutes(){
         updatePagination(1);
         fillRoutesTable();
         fillOptionSelector();
+    }
+}
+
+function checkPeopleNumber(){
+    if (event.target.value > 10){
+        document.getElementById("firstOption").disabled = true;
+        document.getElementById("firstOption").checked = "";
+    } else{
+        document.getElementById("firstOption").disabled = false;
     }
 }
 
